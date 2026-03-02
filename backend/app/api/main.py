01c1e91ec284e19d89e686f9dbea8e2ca38a4dfd
Juan Morais <timrodz@icloud.com>
2026-03-02 22:45:15 +1300

    fix: Import payments service to api router

--- backend/app/api/main.py
+++ backend/app/api/main.py
@@ -1,9 +1,10 @@
 from fastapi import APIRouter
 
-from app.api.routes import users, schools, trips, registrations
+from app.api.routes import users, schools, trips, registrations, payments
 
 api_router = APIRouter()
 api_router.include_router(users.router)
 api_router.include_router(schools.router)
 api_router.include_router(trips.router)
 api_router.include_router(registrations.router)
+api_router.include_router(payments.router)
